1fc692dcc1be216d62977be9966cdbde842e2d4a
unknown <i.am.brady.ward@gmail.com>
2021-02-25 01:44:44 -0500

    Clean up

--- client/src/Items.js
+++ client/src/Items.js
@@ -155,22 +155,30 @@ function Items() {
             </div>
             <Modal show={show} onHide={closeHandler}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Test</Modal.Title>
+                    <Modal.Title>Edit</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                 <Form>
                     <Row className="justify-content-md-center">
-                        <Col className="justify-content-md-center" lg="6">
-                            <Form.Label>Test</Form.Label>
+                        <Col className="text-center" lg="2">
+                            <Form.Label>Name</Form.Label>
+                        </Col>
+                        <Col lg="6">
                             <Form.Control placeholder="Name" name='name' onChange={editingChangeHandler} value={currentEditItem?.name}/>
                         </Col>
                     </Row>
                     <Row className="justify-content-md-center">
+                        <Col className="text-center" lg="2">
+                            <Form.Label>lb</Form.Label>
+                        </Col>
                         <Col lg="6">
                             <Form.Control placeholder="0 lb" name='amountInP' onChange={editingChangeHandler} value={currentEditItem?.amountInP}/>
                         </Col>
                     </Row>
                     <Row className="justify-content-md-center">
+                        <Col className="text-center" lg="2">
+                            <Form.Label>oz</Form.Label>
+                        </Col>
                         <Col lg="6">
                             <Form.Control placeholder="0 Oz" name='amountInOz' onChange={editingChangeHandler} value={currentEditItem?.amountInOz}/>
                         </Col>
