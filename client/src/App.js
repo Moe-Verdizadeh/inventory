df6227a4027be7c73010155b56f53f863e4970a3
Mohammad <mohammad.khanverdizadeh@gmail.com>
2021-02-25 21:27:00 -0500

    user "user" pass "test123"

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './App.css';
 import Header from './components/Header';
 import Items from './pages/Items'; 
 import Locations from './pages/Locations'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences'; 
+import Login from './components/Login/Login'
 
 function App() {
+ 
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  } 
+
   return (
     <div> 
       <Header/> 
       <div >
         <Locations />
-        <Items /> 
+        <Items />  
+        {/* <div className="wrapper">
+          <h1>Application</h1>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/dashboard">
+                <Dashboard />
+              </Route>
+              <Route path="/preferences">
+                <Preferences />
+              </Route>
+            </Switch>
+          </BrowserRouter>
+        </div>  */} 
       </div>
     </div>
   );
