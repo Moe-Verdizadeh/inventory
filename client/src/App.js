151b7142170830d2943edcd216d3ae0646c3699c
Mohammad <mohammad.khanverdizadeh@gmail.com>
2021-03-01 21:24:03 -0500

    added matrial ui for signin and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,22 @@
 import React, { useState }  from 'react';
-import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Items from './pages/Items'; 
-import Locations from './pages/Locations'; 
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from './components/Dashboard/Dashboard';
-import Preferences from './components/Preferences/Preferences'; 
-import Login from './components/Login/Login'
+import './App.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import Dashboard from './components/Dashboard/Dashboard'; 
+import SignUp from  './components/Login/SignUp';
+import SignIn from './components/Login/SignIn'
 
 function App() {
  
   // const [token, setToken] = useState();
   // if(!token) {
-  //   return <Login setToken={setToken} />
+  //   return <SignIn setToken={setToken} />
   // } 
 
   return (
     <div> 
-      <Dashboard />
-      {/* <Header/> 
-      <Sidebar /> */}
-      <div > 
-        {/* <div className="wrapper">
-          <h1>Application</h1>
-          <BrowserRouter>
-            <Switch>
-              <Route path="/dashboard">
-                <Dashboard />
-              </Route>
-              <Route path="/preferences">
-                <Preferences />
-              </Route>
-            </Switch>
-          </BrowserRouter>
-        </div>  */} 
-      </div>
+      <Dashboard /> 
+      {/* <SignIn />
+      <SignUp /> */}
     </div>
   );
 }
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -11,9 +11,7 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container'; 
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
@@ -25,8 +23,8 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" target="_blank" href="https://mokode.ca/">
+        MoKode
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -124,7 +122,7 @@ export default function Dashboard() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+  // const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
     <div className={classes.root}>
@@ -141,7 +139,7 @@ export default function Dashboard() {
             <MenuIcon />
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            Dashboard
+            Inventory Dashboard
           </Typography>
           <IconButton color="inherit">
             <Badge badgeContent={4} color="secondary">
@@ -150,6 +148,7 @@ export default function Dashboard() {
           </IconButton>
         </Toolbar>
       </AppBar>
+
       <Drawer
         variant="permanent"
         classes={{
@@ -167,6 +166,7 @@ export default function Dashboard() {
         <Divider />
         <List>List Two</List>
       </Drawer>
+      
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
