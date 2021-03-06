cef94f78bc2f2828e5381fdde1f936ff5547c8fa
Mohammad <mohammad.khanverdizadeh@gmail.com>
2021-03-06 15:46:34 -0500

    add

--- client/src/components/Login/SignOut.js
+++ client/src/components/Login/SignOut.js
@@ -2,10 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import { Button } from 'react-bootstrap'
 
-export default function SignOut() {
-    async function signOut(e){ 
-        axios.post('/api/login/signOut') 
-    }
+export default function SignOut() { 
+
+        axios.post('/api/login/signOut')  
+        
     return (
         <div>
             <Button>Sign Out</Button>
